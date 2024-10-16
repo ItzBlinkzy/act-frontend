9738a2aaa0aeb9c3781c1d58d3dfafbfec158a0b
ItzBlinkzy <kitey13579@gmail.com>
2024-10-16 16:32:15 +0100

    feat: slightly better hero page

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -3,8 +3,9 @@ import { useNavigate } from "react-router-dom"
 const Hero = () => {
 	const navigate = useNavigate()
 	return (
-		<section className="flex h-screen w-full items-center justify-center py-12 md:py-24 lg:py-32 xl:py-48">
-			<div className="container px-4 md:px-6">
+		<section className="relative flex h-screen w-full items-center justify-center bg-cover bg-no-repeat py-12 md:py-24 lg:py-32 xl:py-48">
+			<div className="absolute inset-0 bg-stockbg bg-cover bg-no-repeat opacity-45"></div>
+			<div className="container z-10 px-4 md:px-6">
 				<div className="flex flex-col items-center space-y-4 text-center">
 					<div className="space-y-2">
 						<h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
