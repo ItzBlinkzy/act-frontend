760d966ca8c6a76be60b419e5cbb4f25284bfe8d
ItzBlinkzy <kitey13579@gmail.com>
2024-11-02 20:11:43 +0000

    fix: typescript error fix

--- src/components/Dashboard/Clients/Client.tsx
+++ src/components/Dashboard/Clients/Client.tsx
@@ -124,7 +124,7 @@ const Client = () => {
 										dataKey="value"
 										label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
 									>
-										{pieChartData.map((entry, index) => (
+										{pieChartData.map((_, index) => (
 											<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
 										))}
 									</Pie>
